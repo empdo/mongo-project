a899325682122484d471f867224f1865409a4749
Emil Essung <emil.essung@live.se>
2020-11-10 22:13:30 +0100

    unknown changes

--- mongo_manager.py
+++ mongo_manager.py
@@ -26,15 +26,15 @@ def print_snippets(lang: str):
 
 
 parser = argparse.ArgumentParser(prog='mongo_manager',
-                                 usage="Will allow you to manage you mongodatabase",
-                                 description="Allows you to manage your mongo database",
-                                 add_help=True
-                                 )
+                                usage="Will allow you to manage you mongodatabase",
+                                description="Allows you to manage your mongo database",
+                                add_help=True
+                                )
 group = parser.add_mutually_exclusive_group()
 group.add_argument('-p', '--push', type=str,
-                   metavar='', help='add path of file to be pushed after "-p"')
+                metavar='', help='add path of file to be pushed after "-p"')
 group.add_argument('-lss', '--list_snippets',
-                   help='list snippets', action="store_true")
+                help='list snippets', action="store_true")
 parser.add_argument('-lang', type=mongo_db.LanguageType,
                     metavar='', help='language for earlier command')
 
