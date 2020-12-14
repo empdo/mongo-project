5c7ffd8a872e4b6f288c214258515a51aaf982a3
Emil Essung <emil.essung@live.se>
2020-12-14 11:21:42 +0100

    did token

--- flask_server.py
+++ flask_server.py
@@ -2,6 +2,7 @@ import mongo_db
 import json
 from flask import Flask, Response
 from flask_cors import CORS
+import requests
 
 from mongo_db import LanguageType
 app = Flask(__name__)
@@ -32,7 +33,12 @@ def get_leaderboard():
 def get_token(code: str = None):
     if not code:
         return Response("Missing code", 401)
-    return "heelo"
+    url = "https://github.com/login/oauth/access_token"
+    obj = {"client_id": "9b3060d8b4ddf1f2a7b8", "client_secret": "b150de451bff4e21764f50181037f57b2f92eb2e", "code": code}
+    headers = {'Accept': 'application/json'}
+    x = requests.post(url, data = obj, headers=headers)
+    return x.text
+
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0")
+    app.run(host = "127.0.0.1")
